e98af1a083fd618bff0fa4410669720418f2bc9f
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2023-04-23 02:05:38 +0100

    add random_crop method

--- meta_transformer/utils.py
+++ meta_transformer/utils.py
@@ -32,12 +32,22 @@ def random_rot90(rng, image):
     )
 
 
+def random_resized_crop(rng, image):
+    """Randomly crop and resize an image."""
+    # TODO doesn't work bc of the random shape of the crop
+    k0, k1 = random.split(rng)
+    delta = random.randint(k0, (2,), 0, 5)
+    image = pix.random_crop(k1, image, (32 - delta[0], 32 - delta[1], 3))
+    image = jax.image.resize(image, (32, 32, 3), method="bicubic")
+    return image
+
+
 def augment_datapoint(rng, img):
     """Apply a random augmentation to a single image. Pixel values are assumed to be in [0, 1]"""
-    rng = random.split(rng, 6)
+    rng = random.split(rng, 7)
     img = pix.random_brightness(rng[0], img, 0.3)
     img = pix.random_contrast(rng[1], img, lower=0.2, upper=3)
-    img = pix.random_saturation(rng[2], img, lower=0, upper=3)
+#    img = pix.random_saturation(rng[2], img, lower=0, upper=3)
     img = pix.random_flip_left_right(rng[2], img)
     img = pix.random_flip_up_down(rng[3], img)
     img = random_rot90(rng[4], img)
